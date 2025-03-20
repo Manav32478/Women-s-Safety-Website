ce6e2d488582d84c8d50270cc584c132e517b67e
Manav Sarvaiya <142085689+Manav32478@users.noreply.github.com>
2025-03-21 00:04:40 +0530

    Delete script.js
    Signed-off-by: Manav Sarvaiya <142085689+Manav32478@users.noreply.github.com>

--- script.js
+++
@@ -1,3 +0,0 @@
-function showEmergencyTips() {
-    alert("🚨 Emergency Tips:\n\n1️⃣ Call 112 (India's Emergency Number)\n2️⃣ Share Your Location with a Friend\n3️⃣ Scream for Help if in Danger\n4️⃣ Run Towards a Crowded Area\n5️⃣ Trust Your Instincts and Act Fast!");
-}
